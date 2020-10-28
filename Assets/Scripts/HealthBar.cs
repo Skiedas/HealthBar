16134edfadaaa724d0fcef5f6eedb06bad8ff613
Skiedas <Skiedas@mail.ru>
2020-10-28 16:15:37 +0300

    Add HealthBar

---
+++ Assets/Scripts/HealthBar.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class HealthBar : MonoBehaviour
+{
+    [SerializeField] private Player _player;
+    [SerializeField] private Slider _slider;
+
+    private void OnEnable()
+    {
+        _player.HealthChanged += OnValueChanged;
+        _slider.value = 1;
+    }
+
+    private void OnDisable()
+    {
+        _player.HealthChanged -= OnValueChanged;
+    }
+
+    private void OnValueChanged(int value, int maxValue)
+    {
+        _slider.DOValue((float)value / maxValue, 1f).Play();
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private Animator _animator;
+    [SerializeField] private int _health;
+
+    private int _currentHealth;
+
+    public int Health => _health;
+
+    public event UnityAction<int, int> HealthChanged;
+
+    private void Start()
+    {
+        _currentHealth = _health;
+    }
+
+    public void ApplyDamage(int value)
+    {
+        if (_currentHealth + value >= 0 && _currentHealth + value <= _health)
+        {
+            _currentHealth += value;
+            HealthChanged?.Invoke(_currentHealth, _health);
+
+            StartCoroutine(Transition(value));
+        }
+    }
+
+    private IEnumerator Transition(int value)
+    {
+        _animator.SetInteger("Value", value);
+
+        yield return new WaitForSeconds(.5f);
+
+        _animator.SetInteger("Value", 0);
+    }
+}
